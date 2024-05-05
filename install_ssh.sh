58a2d8086ba7040d804cf418587d1dc0118234f1
florentgotti <95704867+florentgotti@users.noreply.github.com>
2024-05-05 14:05:14 +0200

    Create install_ssh.sh

---
+++ install_ssh.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Vérifier si l'utilisateur a les droits d'administration
+if [[ $EUID -ne 0 ]]; then
+   echo "Ce script doit être exécuté en tant qu'administrateur" 
+   exit 1
+fi
+
+# Mettre à jour les paquets
+apt update
+
+# Installation d'SSH
+apt install openssh-server -y
+systemctl start ssh
+systemctl status ssh
+
+#Création d'une clef ssh
+ssh-keygen -t rsa -b 4096 -C florent.gotti@etu.univ-st-etienne.fr -y
