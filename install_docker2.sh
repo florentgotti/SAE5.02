e7087a81da7d2805d127bccac0af8ee6a2ce0371
florentgotti <95704867+florentgotti@users.noreply.github.com>
2024-05-05 14:14:00 +0200

    Create install_docker2.sh

---
+++ install_docker2.sh
@@ -0,0 +1,5 @@
+#Crée un playbook ansible permettant d'installer de façon automatisée Docker
+ansible-playbook -i inventory.ini playbook.yml --connection=local
+
+#Vérifie que Docker est installé
+docker version
