b97f94b2ac75fe838239e9223b5a2576a19e68e8
florentgotti <95704867+florentgotti@users.noreply.github.com>
2024-05-05 14:04:32 +0200

    Create install_docker.sh

---
+++ install_docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Vérifier si l'utilisateur a les droits d'administration
+if [[ $EUID -ne 0 ]]; then
+   echo "Ce script doit être exécuté en tant qu'administrateur" 
+   exit 1
+fi
+
+# Mettre à jour les paquets
+apt update
+
+# Installation de Docker à partir d'Ansible
+ansible-galaxy collection install community.docker
+
+echo "L'installation de Docker est terminée."
